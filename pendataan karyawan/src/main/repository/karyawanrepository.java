b16374ac41392ffc43aaa7f7153b652a0170df65
Igriinorant <93275709+Igriinorant@users.noreply.github.com>
2026-02-28 10:53:28 +0700

    Refactor KaryawanRepository interface and add methods

--- pendataan karyawan/src/main/repository/karyawanrepository.java
+++ pendataan karyawan/src/main/repository/karyawanrepository.java
@@ -2,6 +2,10 @@ package com.employeemanagement.repository;
 
 import com.employeemanagement.model.Karyawan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface karyawanrepository extends JpaRepository<Karyawan, Long> {
+public interface KaryawanRepository extends JpaRepository<Karyawan, Long> {
+    List<Karyawan> findByNamaContainingIgnoreCase(String nama);
+    List<Karyawan> findByJabatan(String jabatan);
+    List<Karyawan> findByDivisiId(Long divisiId);
 }
